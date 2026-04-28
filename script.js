3e1bf06045250b719fe828ac7e7405d170931e26
Boby Deshwal <boby43773@gmail.com>
2026-04-28 21:18:22 +0530

    script.js

--- script.js
+++ script.js
@@ -463,11 +463,11 @@ function greet(){
 greet();
 // console.log(message);  //problem ha 
 
-let n=10;
-console.log(window.n);
+// let n=10;
+// console.log(window.n);
 
-var h=20;
-console.log(window.h);
+// var h=20;
+// console.log(window.h);
 
 // let j=10;
 // console.log(window.j);
