df900dc1521d32be9dae2cd7abe7d7a768286e18
Boby Deshwal <boby43773@gmail.com>
2026-05-04 20:03:25 +0530

    script.js

--- script.js
+++ script.js
@@ -552,6 +552,6 @@ setTimeout(()=>{
     console.log("giv me this .");
 },3000);
 
-// setTimeout(()=>{
-//     console.log(" this id .");
-// },3000);
+setTimeout(()=>{
+    console.log(" this id .");
+},3000);
